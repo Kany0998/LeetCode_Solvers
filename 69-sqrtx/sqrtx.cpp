8fa9279dfda3b3b78e6f4de04f3dd5e5170193e4
Spiopka <63583601+Kany0998@users.noreply.github.com>
2025-10-23 20:24:35 +0200

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.25%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     int mySqrt(int x) 
     {
-        int xsqrt = sqrt(x);
-        return xsqrt;    
+        long m = x;
+        while(m*m > x)
+        {
+            m =(m + x/m)/2;
+        }
+        return static_cast<int>(m);    
     }
 };
